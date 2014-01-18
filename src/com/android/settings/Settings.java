5340a1705be2490ee1ddfbc6deb969b7d17cfb88
neighbors28 <neighbors28@naver.com>
2014-01-18 21:39:39 +0900

    Remove id.lock_screen_settings

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -182,7 +182,6 @@ public class Settings extends PreferenceActivity
             R.id.nfc_payment_settings,
             R.id.home_settings,
             R.id.interface_section,
-            R.id.lock_screen_settings,
             R.id.system_settings,
             R.id.privacy_settings_cyanogenmod,
             R.id.button_settings
