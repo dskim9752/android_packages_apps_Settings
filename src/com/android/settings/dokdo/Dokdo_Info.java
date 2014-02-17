a1d853706ddb64559b22b559e426b741a6bc07fb
Jinpyo-laptop <neighbors28@naver.com>
2014-02-17 14:19:33 +0900

    DokdoDream (2/2)

--- src/com/android/settings/dokdo/Dokdo_Info.java
+++ src/com/android/settings/dokdo/Dokdo_Info.java
@@ -49,6 +49,7 @@ public class Dokdo_Info extends PreferenceFragment {
     Preference mEmail;
     Preference mFacebook;
 
+    long[] mHits = new long[4];
     int mDevHitCountdown;
 
     @Override
@@ -82,6 +83,19 @@ public class Dokdo_Info extends PreferenceFragment {
             alertDialog.show();
         } else if (preference == mFacebook) {
             facebook("https://www.facebook.com/projectdokdo");
+        } else if (preference.getKey().equals(KEY_MOD_VERSION)) {
+            System.arraycopy(mHits, 1, mHits, 0, mHits.length-1);
+            mHits[mHits.length-1] = SystemClock.uptimeMillis();
+            if (mHits[0] >= (SystemClock.uptimeMillis()-500)) {
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.setClassName("android",
+                        com.android.internal.app.DokdoDream.class.getName());
+                try {
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.e(LOG_TAG, "Unable to start activity " + intent.toString());
+                }
+            }
         }
         return super.onPreferenceTreeClick(preferenceScreen, preference);
     }
